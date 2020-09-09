50e46936f5e9eb231510267b5b61f0a3f2e9335e
Luke Riley <lukegough@me.com>
2020-09-09 10:54:03 -0700

    constructor
    added constructor and some variables

--- src/LZW.java
+++ src/LZW.java
@@ -1,4 +1,10 @@
-
+import java.util.*;
+import java.io.*;
 public class LZW {
-
+	String inputFile;
+	BufferedReader br;
+	public LZW(String input) throws FileNotFoundException {
+		inputFile = input;
+		br = new BufferedReader(new FileReader(inputFile));
+	}
 }

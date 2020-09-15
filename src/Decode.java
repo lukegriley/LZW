0de3355f43f04a9a844dd6180fa542bb13d0e2ae
JackyZhang1219 <zhangzhengxi1219@gmail.com>
2020-09-15 15:19:11 -0700

    completed convertFileToString Method
    Jacky completed the method in converting the file to a string

--- src/Decode.java
+++ src/Decode.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 public class Decode{
 	public static HashMapString, Integer> getHashMap(String ciphertext)
 	{
@@ -6,6 +8,18 @@ public class Decode{
 	
 	public String convertFileToString(String filename)
 	{
+		//read encoded file as an input
+		BufferedReader encodedFileReader = new BufferedReader(new FileReader(filename));
+		//create the string that will be the decoded file in that format
+		String encodedString = "";
+		int current;
 		
+		//iterate through the encoded file and convert it into a string
+		while((current = encodedFileReader.read())!=-1)
+		{
+			encodedString = encodedString+(char)current;
+		}
+		
+		return encodedString;
 	}
 }

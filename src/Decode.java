b9b749f38b42273748d055c6adb6b2c53996d7dd
JackyZhang1219 <zhangzhengxi1219@gmail.com>
2020-09-16 13:58:24 -0700

    complete method

--- src/Decode.java
+++ src/Decode.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.*;
+
 public class Decode{
 	public static HashMap<String, Integer> getHashMap(String ciphertext)
 	{
+		
+	}
+	
+	public void decodeCipherTextWithHashMap(ArrayList<Integer> encodedInts, String decodedFileName)
+	{
+		HashMap<Integer,String> dictionary= getLZWDecodingHashMap(ArrayList<Integer> encodedInts);
+		
+		File decodedFile = new File(decodedFileName);
+		FileWriter decodeWriter = new FileWriter(decodedFile);
+		
+		for(int i=0;i<dictionary.size();i++)
+		{
+			decodeWriter.write(dictionary.get(encodedInts.get(i)));
+		}
+		decodeWriter.close();
 		
 	}
 }
